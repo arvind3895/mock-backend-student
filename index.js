032c36790e9433f80f3081f5cfe4efd83f3f0c2b
Veerapandian <Veera@MacBook-Pro.local>
2020-08-21 13:24:03 +0530

    added login credentials

--- index.js
+++ index.js
@@ -5,109 +5,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-// const carousel = require("./router/carousel/carousel");
-// const makeAnImpact = require("./router/makeImpact/makeImpact");
-// const recommendAnExchange = require("./router/recommendAnExchange/recommendAnExchange");
-// const investmentDashboard = require("./router/investmentDashboard/investmentDashboard");
-// const myAccount = require("./router/myAccount/myAccount");
-// const investmentOptionUST = require("./router/investmentOptionUST/investmentOptionUST");
-// const investmentOptionDrupal = require("./router/investmentOptionDrupal/investmentOptionDrupal");
-// const dropdownValues = require("./router/npaccount/dropdownValues");
-// const userDetails = require("./router/npaccount/userDetails");
-// const contributionActivity = require('./router/contributionActivity/contributionActivity');
-// const getFundDetails = require('./router/makeAContribution/getFundDetails');
-// const InvestmentAllocation = require('./router/makeAContribution/InvestmentAllocation');
-// const generateAccessKey = require('./router/makeAContribution/generateAccessKey');
-// const loggedInAddress = require('./router/makeAContribution/loggedInAddress');
-// const viewContribution = require('./router/viewContribution/viewContribution');
-// const additionalDetails = require('./router/makeAContribution/findAdditionalDetails');
-// const yearWiseBalance = require('./router/makeAContribution/yearWiseBalance');
-// const step1Info = require('./router/makeAContribution/step1info');
-// const step1Notice = require('./router/makeAContribution/step1Notice');
-// const step2Notice = require('./router/makeAContribution/step1Notice');
-// const disclaimerStep3 = require('./router/makeAContribution/disclaimerStep3');
-// const wiringInstructions = require('./router/makeAContribution/wiringInstructions');
-// const viewContribNotice = require('./router/viewContribution/noticeViewContribution');
-
-// const searchCharity = require("./router/searchCharity/searchCharity");
-// const MyProfile = require("./router/MyProfile/MyProfile");
-// const changeSecurity = require("./router/changeSecurity/changeSecurity");
-// const forgotUserId = require("./router/forgotUserId/forgotUserId");
-// const forgotPasswordAuth = require("./router/forgotPassword/forgotPasswordAuth");
-// const validateChallenge = require("./router/validateChallenge/validateChallenge");
-// const messages = require("./router/messages/messages");
-// const allmessages = require("./router/messages/allmessages");
-
-// const exchanges = require("./router/exchanges/exchanges");
-// const exchangeActivity = require("./router/exchangeActivity/exchangeActivity");
-// const needAssistance = require("./router/needAssistance/needAssistance");
-// const newSingleExchange = require("./router/recommendAnExchange/newSingleExchange/newSingleExchange");
-// const accountSumary = require("./router/accountSummary/accountSummary");
-// const adminSearchData = require("./router/adminSearch/adminSearch");
-// const allPooldata = require("./router/recommendAnExchange/investmentAllDetails/investmentAllDetails");
-// const contributionDashbaord = require("./router/contributionsDashboard/contributionDashboard");
-// const grantsDashboard = require("./router/grantDashboard/grantDashboard");
-// const grantsActivity = require('./router/grantsActivity/grantsActivity');
-// const transactionSummary = require('./router/transactionSummary/transactionSummary');
-// const grantAgreement = require('./router/grantAgreement/grantAgreement');
-// const documentCentre = require('./router/documentcentre/documentcentre');
-// const DrupalContentRouter = require('./router/drupalContents/index');
 const studentDetails = require("./router/student-details/student-details");
 
 app.use(morgan("combined"));
 app.use(cors());
 app.use(express.json());
-//app.use("/viewContribution", viewContribution);
 
 app.use("/", studentDetails);
-// app.use('/', carousel)
-// app.use('/',grantsActivity)
-// app.use('/',exchangeActivity)
-// app.use('/',exchanges);
-// app.use('/', makeAnImpact)
-// app.use('/search', searchCharity)
-// app.use('/', recommendAnExchange)
-// app.use('/', allPooldata)
-// app.use('/', investmentDashboard)
-// app.use('/', myAccount)
-// app.use('/', investmentOptionUST)
-// app.use('/', investmentOptionDrupal)
-// app.use('/contributionActivity', contributionActivity)
-// app.use('/makeAContribution/allocations',InvestmentAllocation)
-// app.use('/makeAContribution/fundDetails',getFundDetails)
-// app.use('/makeAContribution/accessKey',generateAccessKey)
-// app.use('/makeAContribution/Address',loggedInAddress)
-// app.use('/makeAContribution/additional',additionalDetails)
-// app.use('/makeAContribution/Balance',yearWiseBalance)
-// app.use('/makeAContribution/info', step1Info)
-// app.use('/documentCentre', documentCentre)
-// app.use('/npaAccount/userDetails',userDetails)
-// app.use('/npaAccount/dropdownValues',dropdownValues)
-// app.use('/notices/PageMake_a_Contribution', step1Notice)
-// app.use('/notices/PageMake_a_Contribution2', step2Notice)
-// app.use('/notices/PageView_Contribution',viewContribNotice)
-// app.use('/disclaimers/PageMake_a_Contribution3', disclaimerStep3)
-// app.use('/api/contribution-flow/NameWiring-Instructions', wiringInstructions)
-// app.use('/', MyProfile)
-// app.use('/',changeSecurity)
-// app.use('/', forgotUserId)
-// app.use('/', forgotPasswordAuth)
-// app.use('/', validateChallenge)
-// app.use('/', needAssistance)
-// app.use('/', messages)
-// app.use('/',allmessages)
-// app.use('/', newSingleExchange)
-// app.use('/', adminSearchData)
-// app.use('/', transactionSummary)
-// app.use('/', accountSumary)
-// app.use('/documentCentre', documentCentre);
-// app.use('/grantAgreement', grantAgreement);
-// app.use("/", contributionDashbaord);
-// app.use("/", grantsDashboard);
-// app.use('/viewContribution',viewContribution)
-// app.use('/contributionActivity', contributionActivity)
-// app.use('/grantAgreement', grantAgreement)
-// app.use('/', DrupalContentRouter);
-
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- router/student-details/student-details.js
+++ router/student-details/student-details.js
@@ -11,20 +11,42 @@ router.get("/school/student-details", function (req, res) {
       if (err) {
         return console.log(err);
       }
-      res.send(data);
+      res.status(200).send(data);
     }
   );
 
   var options = rootPath("public/school/studentDetails");
   var fileName = "studentDetails.json";
-  //   res.sendFile(fileName, options, (err) => {
-  //     console.log(err, "err");
-  //     if (err) {
-  //       next(err);
-  //     } else {
-  //       console.log("Sent:", fileName);
-  //     }
-  //   });
 });
+
+router.get("/school/users", (req, res) => {
+  fs.readFile("public/school/studentDetails/users.json",
+  "utf8", (err,data) => {
+    if(err) {
+      return console.log(err);
+    }
+    res.status(200).send(data)
+  });
+});
+
+router.post('/login', (req, res) => {
+    const userDetail = req.body;
+    fs.readFile("public/school/studentDetails/users.json",
+    "utf8", (err,data) => {
+      if(err) {
+        return console.log(err);
+      }
+      const loginCred = (JSON.parse(data)).find(user => {
+        if((user.name === userDetail.name || user.email === userDetail.email) && user.password === userDetail.password) {
+          return true;
+        }
+      });
+
+      if(!loginCred) {
+        res.status(400).send({error: 'Invalid UserName or password'});
+      }
+      res.status(200).send({message: 'Successfully loggedin',...loginCred})
+    });
+  })
 
 module.exports = router;
