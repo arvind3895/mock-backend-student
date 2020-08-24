1e227c22b3eda869beabf042cdd1398ccd016b4d
Veerapandian <Veera@MacBook-Pro.local>
2020-08-24 19:16:20 +0530

    updated put and delete requests

--- router/student-details/student-details.js
+++ router/student-details/student-details.js
@@ -47,6 +47,92 @@ router.post('/login', (req, res) => {
       }
       return res.status(200).send({message: 'Successfully loggedin',...loginCred})
     });
+  });
+
+  router.post('/addUser', (req, res) => {
+    const newUser = req.body;
+    var users;
+    var userData;
+    if (req.body === '') {
+        return res.status(400).send({message: 'Invalid body data'});
+    }
+    fs.readFile('public/school/studentDetails/users.json', "utf8", (err, data) => {
+        if (err) {
+            return console.log(err);
+        }
+        users = JSON.parse(data);
+
+        userData = users.find(user => {
+            if(newUser.name === user.name || newUser.email === user.email) {
+                return true;
+            }
+        });
+
+        if(userData) {
+            return res.status(400).send({error: 'User name / email already exists'});
+        }
+        
+        users.push(newUser);
+
+        fs.writeFile('public/school/studentDetails/users.json', JSON.stringify(users) , err => {
+            if (err) {
+                return console.log(err);
+            }
+            fs.readFile('public/school/studentDetails/users.json', "utf8", (err, data) => {
+                if (err) {
+                    return console.log(err);
+                }
+                res.status(200).send(JSON.parse(data));
+            });
+        })
+    });
+    
+});
+
+router.put('/changePassword', (req,res) => {
+  const newRequest = req.body;
+  var userDetails;
+  if (req.body === '') res.status(400).send({message: 'Invalid body data'});
+  fs.readFile('public/school/studentDetails/users.json', (err, data) => {
+      if (err) return console.log(err);
+      userDetails = JSON.parse(data);
+      const index = userDetails.findIndex(user => user.email === newRequest.email);
+      userDetails[index].password = newRequest.password;
+      fs.writeFile('public/school/studentDetails/users.json', JSON.stringify(userDetails) , err => {
+          if (err) {
+              return console.log(err);
+          }
+          fs.readFile('public/school/studentDetails/users.json', "utf8", (err, data) => {
+              if (err) {
+                  return console.log(err);
+              }
+              res.status(200).send(JSON.parse(data));
+          });
+      });
+  });
+});
+
+router.delete('/removeUser', (req,res) => {
+  const newRequest = req.body;
+  var userDetails;
+  if (req.body === '') res.status(400).send({message: 'Invalid body data'});
+  fs.readFile('public/school/studentDetails/users.json', (err, data) => {
+      if (err) return console.log(err);
+      userDetails = JSON.parse(data);
+      const index = userDetails.findIndex(user => user.email === newRequest.email);
+      userDetails.splice(index, 1);
+      fs.writeFile('public/school/studentDetails/users.json', JSON.stringify(userDetails) , err => {
+          if (err) {
+              return console.log(err);
+          }
+          fs.readFile('public/school/studentDetails/users.json', "utf8", (err, data) => {
+              if (err) {
+                  return console.log(err);
+              }
+              res.status(200).send(JSON.parse(data));
+          });
+      })
   })
+});
 
 module.exports = router;
